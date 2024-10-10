d55788094869869bf1f57108bec1724f53814fe4
RanjithDevwemo <ranjithdevwemo2@gmail.com>
2024-10-10 18:55:15 +0530

    new

--- retail/src/dbConfig/dbConfig.js
+++ retail/src/dbConfig/dbConfig.js
@@ -1,31 +1,3 @@
-// // dbConfig.ts
-// import mongoose from 'mongoose';
-
-// let isConnected: boolean = false; // Connection state
-
-// export async function Connect(tenantId: string) {
-//     const MONGO_URI = `mongodb+srv://ranjithdevwemo2:ranjithdevwemo2@cluster0.3ckmctb.mongodb.net/${tenantId}`;
-
-//     if (isConnected) {
-//         console.log(`Already connected to database: ${tenantId}`);
-//         return;
-//     }
-
-//     try {
-//         await mongoose.connect(MONGO_URI);
-//         isConnected = true;
-//         console.log(`MongoDB connected successfully to database: ${tenantId}`);
-//     } catch (error) {
-//         console.error('MongoDB connection error:', error);
-//         process.exit(1);
-//     }
-// }
-
-
-
-
-
-
 // dbConfig.js
 const mongoose = require('mongoose');
 
